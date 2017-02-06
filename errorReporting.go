c051a6c5c0456aa09ef16c0c851f4c2c6978fc53
Mike Bell <mike@mikebell.org>
2017-02-06 12:57:44 -0800

    New function

--- errorReporting.go
+++ errorReporting.go
@@ -1,1 +1,19 @@
 package slack
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+)
+
+func (c *SlackChannel) Warning(format string, stuff ...interface{}) {
+	var callingfuncline string
+	log.Printf(format, stuff...)
+	x := fmt.Errorf(format, stuff...)
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		callingfuncline = fmt.Sprintf("%s:%d", file, line)
+	}
+	go c.SendError(x, "warning", map[string]string{"Caller": callingfuncline}, nil)
+	//"Server": serverName,
+}
